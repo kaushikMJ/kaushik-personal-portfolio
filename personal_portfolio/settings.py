e7a98ffbfc689080763a91db22165464c1ecd453
Kaushik Mahajan <voicekaushik@gmail.com>
2022-06-26 21:12:36 +0530

    Fifth push

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u4m(^qye^4$cq=23gywh2z+62o&h=zxd!%*t52fu0szh=11(dx'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
